ddcaac63abda247fb878177ce6260865d97552c2
Jason Wray <jason@kulturny.com>
2015-05-07 17:24:15 -0700

    Add stub MKMapView+Bounds category

---
+++ treble/Additions/MKMapView+Bounds.h
@@ -0,0 +1,13 @@
+//
+//  MKMapView+Bounds.h
+//  Treble
+//
+//  Created by Jason Wray on 5/4/15.
+//  Copyright (c) 2015 Mapbox. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKMapView (Bounds)
+
+@end
---
+++ treble/Additions/MKMapView+Bounds.m
@@ -0,0 +1,15 @@
+//
+//  MKMapView+Bounds.m
+//  Treble
+//
+//  Created by Jason Wray on 5/4/15.
+//  Copyright (c) 2015 Mapbox. All rights reserved.
+//
+
+#import "MKMapView+Bounds.h"
+
+@implementation MKMapView (Bounds)
+
+
+
+@end
