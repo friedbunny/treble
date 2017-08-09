b2a10e6beeb06eb6e0af55184d3f0f7cf6d8cea6
Jason Wray <jason@kulturny.com>
2017-08-08 23:35:36 -0400

    Reorder Mapbox styles — nav last

--- treble/TRBLMapboxMapView.m
+++ treble/TRBLMapboxMapView.m
@@ -135,15 +135,15 @@
     dispatch_once(&onceToken, ^{
         _styles = @[
             [MGLStyle streetsStyleURLWithVersion:10],
-            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-preview-day-v2"],
-            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-guidance-day-v2"],
             [MGLStyle outdoorsStyleURLWithVersion:10],
             [MGLStyle lightStyleURLWithVersion:9],
             [MGLStyle darkStyleURLWithVersion:9],
-            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-preview-night-v2"],
-            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-guidance-night-v2"],
             [MGLStyle satelliteStyleURLWithVersion:9],
             [MGLStyle satelliteStreetsStyleURLWithVersion:10],
+            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-preview-day-v2"],
+            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-guidance-day-v2"],
+            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-preview-night-v2"],
+            [NSURL URLWithString:@"mapbox://styles/mapbox/navigation-guidance-night-v2"],
         ];
     });
     
